f0774cf9c3596f61d1c2ab7e880eff66bff437d9
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-14 18:15:57 +0700

    connect decompile features

--- app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -2,11 +2,14 @@ package com.dicoding.picodiploma.mycamera
 
 import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.content.FileProvider
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
@@ -16,6 +19,7 @@ import java.util.Locale
 
 private const val FILENAME_FORMAT = "yyyyMMdd_HHmmss"
 private val timeStamp: String = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(Date())
+private const val MAXIMAL_SIZE = 1000000
 
 fun getImageUri(context: Context): Uri {
     var uri: Uri? = null
@@ -52,7 +56,7 @@ fun createCustomTempFile(context: Context): File {
     return File.createTempFile(timeStamp, ".jpg", filesDir)
 }
 
-fun uriToFile(imageUri: Uri, context: Context): File{
+fun uriToFile(imageUri: Uri, context: Context): File {
     val myFile = createCustomTempFile(context)
     val inputStream = context.contentResolver.openInputStream(imageUri) as InputStream
     val outputStream = FileOutputStream(myFile)
@@ -69,6 +73,15 @@ fun uriToFile(imageUri: Uri, context: Context): File{
 fun File.reduceFileImage(): File {
     val file = this
     val bitmap = BitmapFactory.decodeFile(file.path)
-
+    var compressQuality = 100
+    var streamLength: Int
+    do {
+        val bmpStream = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+        val bmpPicByteArray = bmpStream.toByteArray()
+        streamLength = bmpPicByteArray.size
+        compressQuality -= 5
+    } while (streamLength > MAXIMAL_SIZE)
+    bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
     return file
 }
